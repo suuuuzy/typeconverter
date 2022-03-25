3ea50d5f335c977ccb49fcfa6bc082b4bfc59585
suuuuzy <jangsooji91@gmail.com>
2022-03-25 19:09:22 +0900

    포맷터 적용

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //컨버터가 포맷터보다 우선순위가 높기 때문에 주석 처리를 하지 않으면 컨버터가 적용된다.
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
 
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
